657601218c409c6cdfebda5f2347921dc7e2c9e7
gabizoca1209 <164278644+gabizoca1209@users.noreply.github.com>
2024-09-23 23:02:41 +0000

    23/09/2024

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-let idade = 17;
-const anoNascimento = 2006; 
-let anoAtual = 2024; 
-const nomeCompleto = "Gabriella Kaipper";
+const prompt = require('prompt-sync')
+  const entrada = prompt('');
 
-console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
+const nomeCompleto = "Gabriella Kaipper ";
+const anoNascimento = 2006;
+let anoAtual = 2024;
+let idade = 18;
+const anoFormatura = 2029;
 
-anoAtual = anoAtual + 1; 
-
-console.log(anoAtual);
+console.log("olá eu sou " +  nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " anos " + " e nasci no ano de " + anoNascimento);
 
 idade = idade + 1;
-console.log(idade);    
+anoAtual = anoAtual + 1;
+console.log("Eu vou fazer " + idade + " anos, ano que vem, em " + anoAtual + ". Quero estar cursando odontologia, e pretendo me formar em " + anoFormatura);
 
- //Agencia de viagens Nova york Rio de Janeiro Grecia  Canada Londres 
+
+//Agência de viagens Hawaii Paris China Russia Cuba
 const listaDeDestinos = new Array(
-  "Nova York",
-  "Rio de Janeiro",
-  "Grecia",
-  "Canada", 
-  "Londres"
-  );
-const idadeComprador = 16;
-
-if(idadeComprador >=18) {
+  "hawaii",
+  "russia",
+  "china",
+  "Paris",
+  "cuba"
+);
+var idadeComprador = entrada("Qual sua idade?");
+var nomeComprador = entrada("Qual seu nome?");  
+if(idadeComprador >= 18) {
+  console.log("Ola Senhor(a) " + nomeComprador)
   console.log("Comprador maior de idade");
-  consolc.log("Destinos disponiveis:" + listaDeDestinos);
-} else {
-  console.log("Infelizmente voce é menor de idade")
-  console.log("Voce nao possui nenhum destino disponivel:");
+  console.log("Escolha seu destino");
+  
+  var contador= 0;
+  
+  while(contador <9){
+  console.log(listaDeDestinos[contador]);  
+  contador++;
+
+
+  console.log("Destinos disṕniveis: " + listaDeDestinos)
+
+;
 }
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
+console.log(listaDeDestinos);
+listaDeDestinos.push("Campo Mourão");
+console.log*(listaDeDestinos);
+listaDeDestinos.push("Ponta grossa");
+console.log(listaDeDestinos);
+listaDeDestinos.push("Cabo Frio");
+console.log(listaDeDestinos);
 
-
+if(idadeComprador <= 18)
+console.log("O senhor(a))não é maior de idade")
+  
+}
+var destino = entrada("Digite o numero que corresponde ao destino selecionado (0-8)" );
+console.log(`destino selecionado foi: 
+${listaDeDestinos[destino]}`);
+}
 
 
